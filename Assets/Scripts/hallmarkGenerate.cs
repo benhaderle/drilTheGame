038e362d355a5dfaa98f6c79ecfef37ae980534a
benhaderle <benhaderle@gmail.com>
2017-04-11 16:25:26 -0400

    Added materials + lights to tile prefab, finalized tile generation

--- Assets/Scripts/hallmarkGenerate.cs
+++ Assets/Scripts/hallmarkGenerate.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class hallmarkGenerate : MonoBehaviour {
 
 	public Transform tileFab;
-	int storeLength;
+	public int storeLength = 12;
 	public Transform player;
 	List<Transform> tileList = new List<Transform>();
 
 	// Use this for initialization
 	void Start () {
-		storeLength = 12;
 		//player.transform.position = new Vector3 (0, 0, 0);
 		for (int i = 0; i < storeLength; i++) {
 			Transform nextTile = Instantiate (tileFab, transform.position, Quaternion.identity);
