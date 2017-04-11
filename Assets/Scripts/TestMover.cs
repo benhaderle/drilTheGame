024922d3cc99e2f3a59830867d7d257ce2b72b3b
Molly <molly354@comcast.net>
2017-04-11 13:46:27 -0400

    more ending stuff

---
+++ Assets/Scripts/TestMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMover : MonoBehaviour {
+	GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate (0f, 0f, 10f*Time.deltaTime);
+		}
+	}
+}
