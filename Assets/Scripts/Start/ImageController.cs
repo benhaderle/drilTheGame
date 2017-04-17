4da4426be0ee408755942aff8ce10836e5b4302b
benhaderle <benhaderle@gmail.com>
2017-04-17 14:33:41 -0400

    made a start menu, changed file structure to accomodate multiple scenes

---
+++ Assets/Scripts/Start/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour {
+
+    bool growing = true;
+
+	// Update is called once per frame
+	void Update () {
+        if (growing) {
+            transform.localScale = transform.localScale + (Vector3.one * .01f * Time.deltaTime);
+            if (transform.localScale.x >= 1.3f)
+                growing = false;
+        }
+        else {
+            transform.localScale = transform.localScale - (Vector3.one * .01f * Time.deltaTime);
+            if (transform.localScale.x <=  1f)
+                growing = true;
+        }
+	}
+}
