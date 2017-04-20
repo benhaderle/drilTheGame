c81427ab313391c40c4ee528dfe46278e235446b
benhaderle <benhaderle@gmail.com>
2017-04-20 15:10:52 -0400

    made hall wider, made necessary adjustments

--- Assets/Scripts/Jail/hallmarkGenerate.cs
+++ Assets/Scripts/Jail/hallmarkGenerate.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class hallmarkGenerate : MonoBehaviour {
 
 	public Transform tileFab;
-	public int storeLength = 12;
+	public float storeLength = 12;
+    public int numTiles;
 	List<Transform> tileList = new List<Transform>();
 
 	// Use this for initialization
 	void Start () {
 		//player.transform.position = new Vector3 (0, 0, 0);
-		for (int i = 0; i < storeLength; i++) {
+		for (int i = 0; i < numTiles; i++) {
 			Transform nextTile = Instantiate (tileFab, transform.position, Quaternion.identity);
-			transform.position = new Vector3 (0, 0, transform.position.z + 20);
+			transform.position = new Vector3 (0, 0, transform.position.z + storeLength);
 			tileList.Add (nextTile);
 		}
 	}
@@ -25,6 +26,6 @@ public class hallmarkGenerate : MonoBehaviour {
 
 	void GenerateNext() {
 		Instantiate (tileFab, transform.position, Quaternion.identity);
-		transform.position = new Vector3 (0, 0, transform.position.z + 20);
+		transform.position = new Vector3 (0, 0, transform.position.z + storeLength);
 	}
 }
