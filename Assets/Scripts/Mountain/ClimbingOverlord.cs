9375d3173af984a8f82969d8dbab293ea30f2a45
Gaberino <gr1157@nyu.edu>
2017-04-20 16:46:00 -0400

    Added Base Mountain Climbing Mechanics
    Who's laughing now Christopher Columbus

---
+++ Assets/Scripts/Mountain/ClimbingOverlord.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbingOverlord : MonoBehaviour {
+
+	public static ClimbingOverlord Instance;
+	public GameObject Climber;
+
+	public GameObject climbForceSphere;
+	public float maxSphereDistanceFromPlayer;
+
+	public float clickBufferTime = 0.5f;
+	public float currentClickTimer = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentClickTimer += Time.deltaTime;
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit mouseHit;
+		if (Physics.Raycast(mouseRay, out mouseHit)){
+			if (Vector3.Distance (Climber.transform.position, mouseHit.point) < maxSphereDistanceFromPlayer){
+				climbForceSphere.transform.position = mouseHit.point;
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0)){
+			if (ClimbSphere.Instance.touchingLimb){
+				currentClickTimer = 0f;
+				ClimbSphere.Instance.AttachToLimb();
+			}
+		}
+		if (Input.GetMouseButtonUp(0)){
+			ClimbSphere.Instance.Detach();
+		}
+	}
+}
