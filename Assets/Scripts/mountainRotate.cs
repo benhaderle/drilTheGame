7618a28ef7e8b9b62947d21f016f860ff53b4d33
Unknown <sp3990@nyu.edu>
2017-04-25 13:15:43 -0400

    metal hit & wood break sounds, prototyped mountain

---
+++ Assets/Scripts/mountainRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mountainRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.eulerAngles = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//TAKE THIS OUT WHEN YOU PUT THIS IN THE ACTUAL SCENE 
+		if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 2, transform.eulerAngles.z);
+
+		}
+	}
+}
