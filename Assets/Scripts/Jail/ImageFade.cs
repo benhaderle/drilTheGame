53113ea46ff1dd4f774ee6d83a97f9378fc2d6e2
benhaderle <benhaderle@gmail.com>
2017-05-10 18:07:23 -0400

    added fade between start and jail

---
+++ Assets/Scripts/Jail/ImageFade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour {
+
+    Image image;
+    float alpha;
+    public bool fadeIn = true;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        alpha = image.color.a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(fadeIn)
+            alpha += .2f * Time.deltaTime;
+        else
+            alpha -= .2f * Time.deltaTime;
+        image.color = new Color(0, 0, 0, alpha);
+    }
+}
