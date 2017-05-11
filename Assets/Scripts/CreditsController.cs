1635bafaf1d6b54ab9d50fabed592943f649a27d
Gaberino <gr1157@nyu.edu>
2017-05-10 21:28:13 -0400

    Added rudimentary credits

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour {
+	public CameraCinematic CineToRun;
+
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine(CineToRun.ExecuteCinematic(true));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
